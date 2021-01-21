b7c6261878b8521a5d9fc1209fd566de38fd5609
klimenkoff9 <dennis.klimenkov9@gmail.com>
2021-01-21 15:10:00 -0500

    Refracture repo

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+    //   <Switch>
+    
+    //   </Switch>
+    <h1>Hello World</h1>
+    );
+  };
+  
+  export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+
+import Routes from "./Routes"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+     <Router>
+        {/* <header>
+          <Navbar />
+        </header> */}
+
+      <main>
+        <Routes />
+      </main>
+
+      {/* <footer>
+        <Footer />
+      </footer> */}
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
