5f33ed8f7017aea903c0d7f9a5a8a3f824e93af5
dkamalov <dkamalov8@gmail.com>
2021-01-22 15:28:06 -0500

    Campus name and address

--- src/pages/Campuses/components/AddCampus.jsx
+++ src/pages/Campuses/components/AddCampus.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Button from'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Campus from "./CampusArray/components/NewCampus";
+import campusesList from "./CampusesArray.jsx";
+
 class AddCampus extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {name: ''};
-    
+        this.state = {
+            name: '', 
+            address: ''
+        };
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeA = this.handleChangeA.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        
       }
     
       handleChange(event) {
         this.setState({value: event.target.value});
       }
+
+      handleChangeA(event) {
+        this.setState({address: event.target.value});
+      }
     
       handleSubmit(event) {
-        alert('The Campus ' + this.state.value + " has been added.");
+        alert('The Campus ' + this.state.value + " at " + this.state.address + " has been added.");
         event.preventDefault();
+        
       } 
 
     render(){
@@ -27,10 +35,17 @@ class AddCampus extends React.Component {
             
             <form onSubmit={this.handleSubmit}>
                <div class = "form-group">
+
                    <div class="col-7">
                    <label for = "campusname"></label>
                    <input type = "username" class="form-control form-control-lg" id="exampleInputPassword1" onChange={this.handleChange} placeholder="Campus Name"></input>
-               </div>  
+                   </div>  
+
+                   <div class="col-7">
+                   <label for = "address"></label>
+                   <input type = "username" class="form-control form-control-lg" id="exampleInputPassword1" onChange={this.handleChangeA} placeholder="Campus Address"></input>
+                   </div>  
+
                </div>  
                <div class = "text-center"> 
              <button type="submit" class = "btn btn-primary"> Add Campus</button>
