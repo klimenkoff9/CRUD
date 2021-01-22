bcd15752cd18999d850c5585d350e4479f17207e
dkamalov <dkamalov8@gmail.com>
2021-01-22 10:07:24 -0500

    first

--- src/pages/Campuses/Campuses.jsx
+++ src/pages/Campuses/Campuses.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-
 import CampusesArray from "./components/CampusesArray";
 
-const Campuses = () => {
-  return (
-    <div className="Contact">
-      <article>
-        <CampusesArray />
-      </article>
-    </div>
-  );
-};
+import React, { Component } from "react"
+
+class Campuses extends Component {
+
+    render(){
+
+        console.log("Render Campus Listing Blank Slate");
+        return(
+
+            <div>
+                <h1> Campus Listing Blank Slate </h1>
+                <hr></hr>
+
+              <div>
+                    <center>
+                      <h2> All Campuses</h2> 
+                    </center>
+             </div>
+
+                {/* Navbar in between h1 and h2. 
+                
+                To display campuses we'll have a list */ }
+                <CampusesArray />
+
+               <center>  
+                   <button> Add Campus </button> 
+                </center>
+
+            </div>
+
+        );
+    }
+}
 
 export default Campuses;
