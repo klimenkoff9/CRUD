b2d5542b80adc10ec7a3c0b4cf2ae7e8da555af0
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-21 19:25:51 -0500

    make campuses into a class and modified allplayers.js to allCampuses.js

--- src/pages/Campuses/Campuses.jsx
+++ src/pages/Campuses/Campuses.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchAllCampusesThunk, FETCH_ALL_CAMPUSES } from "../../store/utilities/allCampuses";
 
-const Campuses = () => {
-  return <h1>Hello Campuses</h1>;
+class Campuses extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello Campuses</h1>
+      </div>
+    )
+  }
 };
 
-export default Campuses;
+const matchStateToProps = (state) => {
+  console.log( `MATCH STATE TO PROPS` );
+  return{
+    campuses : state.campuses
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  console.log(`MAP DISPATCH TO PROPS`);
+  return {
+    getAllCampuses : () => dispatch(fetchAllCampusesThunk())
+  }
+}
+
+
+
+export default connect( matchStateToProps, mapDispatchToProps )( Campuses );
