a4a3c23890a2fa79bdf51c6af3242a5974bdfe40
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2021-01-23 14:47:10 -0500

    added student info page which can be directed to campus info page

--- src/pages/Students/Student.jsx
+++ src/pages/Students/Student.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import "./student.css"
 
@@ -6,7 +7,7 @@ const Student = ( props ) => {
     return (
         <div className = "student-card">
             <img src={ props.imageUrl } alt="student-profile" width = "100px" height = "80px"></img>
-            <p>{ props.firstName } { props.lastName }</p>
+            <Link to = {`/student/${props.StudentId}`}><p>{ props.firstName } { props.lastName }</p></Link>
             <p>{ props.campusname }</p>
         </div>
     )
