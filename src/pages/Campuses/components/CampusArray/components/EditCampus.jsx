afc19e5a5a596f866aff6f501b28ad002bd03a09
Andrew Previl <drewprevil@gmail.com>
2021-01-23 16:06:35 -0500

    not ready to merge

--- src/pages/Campuses/components/CampusArray/components/EditCampus.jsx
+++ src/pages/Campuses/components/CampusArray/components/EditCampus.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import Form from 'react-bootstrap/Form';
-//import Button from 'react-boostrap/Button';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import StudentList from './studentlist.jsx'
+
 
 class editCampus extends Component{
     constructor(props){
@@ -9,7 +13,8 @@ class editCampus extends Component{
             image:"",
             name:"",
             location:"",
-            description:""
+            description:"",
+            testArray:["bob","mary","joe","betty","sally","fred","chris","larry"]
         }
     }
     handleChange=(e)=>{
@@ -21,7 +26,7 @@ class editCampus extends Component{
         alert("state: "+this.state.image+"\n" +
             this.state.name+"\n"
             +this.state.location+"\n"+this.state.description);
-
+        this.updateCampus();
         //clear state
         // this.setState({image:"",
         // name:"",
@@ -29,38 +34,38 @@ class editCampus extends Component{
         // description:""})
         e.preventDefault();
     }
+    async updateCampus(){
+        const campus = await axios.post(`http://localhost8080/api/campus/1`,{name: this.state.name})
+        console.log(campus);
+    }
     render(){
        return( 
        <div>
            <h1>Edit Campus</h1>
-            <form onSubmit = {this.onSubmit}>
-                <label>Campus Name
-                <input type = "text" name="name" value={this.state.name} onChange = {this.handleChange}></input><br/>
-                </label>
-                
-                <label>Campus Location
-                <input type = "text" name="location" value={this.state.location} onChange = {this.handleChange}></input><br/>
-                </label>
-                
-                <label> Campus Image URL
-                <input type = "text" name = "image" value={this.state.image}  onChange = {this.handleChange}></input><br/>
-                </label>
-                
-                <label> Campus Description
-                <input type = "text" name="description" value={this.state.description} onChange = {this.handleChange}></input><br/>
-                </label>
 
-                <input type ="submit" value = "Save Changes"/>
-            </form>
-            <form>
-                <label>
-                    Students on Campus<br/>
-                    <select>
-                        {/* drop down list of campus */}
-                    </select>
-                </label>
-            </form>
-        </div>
+           <Form onSubmit = {this.onSubmit} >
+               <Form.Group>
+                   <Form.Label>Campus name</Form.Label>
+                       <Form.Control  type = "text" name="name" value={this.state.name} onChange = {this.handleChange}/>
+               </Form.Group>
+               <Form.Group>
+                   <Form.Label>Campus Location</Form.Label>
+                       <Form.Control  type = "text" name="location" placeholder={this.props.id} value={this.state.location} onChange = {this.handleChange}/>
+               </Form.Group>
+               <Form.Group>
+                   <Form.Label>Campus Image URL</Form.Label>
+                       <Form.Control  type = "text" name = "image" value={this.state.image}  onChange = {this.handleChange}/>
+               </Form.Group>
+               <Form.Group>
+                   <Form.Label>Campus Description</Form.Label>
+                       <Form.Control  type = "text" name="description" value={this.state.description} onChange = {this.handleChange}/>
+               </Form.Group>
+               <Button type="submit" > Save Changes </Button>
+
+           </Form>
+
+            <StudentList student = {this.state.testArray}/>
+        </div> 
     )}
 }
 export default editCampus;
---
+++ src/pages/Campuses/components/CampusArray/components/studentlist.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class student extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            studentAdd:"temp",
+            studentArray: this.props.student
+        }
+    }
+
+    onSubmit =(e) =>{
+        
+        this.setState=({
+            studentAdd: "test"
+        })
+        alert(this.state.studentAdd)
+        e.preventDefault()
+    } 
+    handleChange=(e)=>{  
+        let name = this.props.student[e.target.value];
+        {console.log(this.props.student[e.target.value])}
+        {console.log(this.state.studentArray[e.target.value])}
+        this.setState=({
+            studentAdd: name
+        })
+        {console.log(this.state.studentAdd)}   
+        
+    }
+    render(){
+        return(   
+            <Form onSubmit={this.onSubmit}>
+                <Form.Group>
+                    <Form.Label>Students on Campus</Form.Label>
+                        <Form.Control as="select" onChange={this.handleChange}>             
+                            {this.props.student.map((student,index)=>{                  
+                                return <option value ={index}>{index} {student}</option>
+                            })}
+                        </Form.Control>                 
+                </Form.Group>
+                <Button type="submit">Add to Campus</Button>     
+            </Form>
+        );
+    }
+}
+export default student;
