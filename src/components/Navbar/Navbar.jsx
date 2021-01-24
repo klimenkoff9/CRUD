aaee365d14eca39b2a0cd9f492a59d5342772b70
Andrew Previl <drewprevil@gmail.com>
2021-01-24 16:07:24 -0500

    homepage css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Form, FormControl, Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import './Navbar.css'
 
-const Navbar = () => {
+const Navbarr = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/campuses">Campuses</Link>
-        </li>
-        <li>
-          <Link to="/students">Students</Link> 
-        </li>
-        
-      </ul>
-    </nav>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">Campus Listing</a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <Link to="/">Home </Link>
+          </li>
+          <li class="nav-item">
+            <Link to="/campuses">Campuses</Link>
+          </li>
+          <li class="nav-item"> 
+            <Link to="/students">Students</Link>          
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   );
 };
 
-export default Navbar;
+export default Navbarr;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => <center><h1>Welcome to CUNY University! </h1></center>;
+const Home = () => {
+    return(
+        <div>
+            
+            <center><img src="https://www.cuny.edu/wp-content/uploads/sites/4/media-assets/skyline-1200x627_CUNY_Logo.jpg"/></center><br></br>
+            
+            <center><h1>Welcome to CUNY University! </h1></center>
+            <hr></hr>
+        </div>
+        );
 
-
+}
 
 export default Home;
