2f4b3e1d9a95b4ed33dd2dc8ff018e35752721bd
dkamalov <dkamalov8@gmail.com>
2021-01-24 22:47:19 -0500

    below banner Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,22 +2,57 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
+
     return(
         <div>
-            
-            <center><img src="https://www.cuny.edu/wp-content/uploads/sites/4/media-assets/skyline-1200x627_CUNY_Logo.jpg"/></center><br></br>
-            
-            <center><h1>Welcome to CUNY University! </h1></center>
+                <center><img src="https://www.cuny.edu/wp-content/uploads/sites/4/media-assets/skyline-1200x627_CUNY_Logo.jpg"/></center><br></br>
+                
+                <center><h1>Welcome to CUNY!</h1></center>
 
-            <hr></hr>
+                <hr></hr>
 
-            <img src="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/home-preview/include-news/year_in_review.png"></img>
 
-            <p float="right"> Giving out vaccines and shit </p>
+            <div class="row">
 
+                <div class = "column">
+                <center>
+                    <img src="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/global/Student_Awards_891x891.jpg" width="600" height="500"></img>
+                </center>
+                    <hr class="goldDec"></hr>
+                <div class= "columnAdj">
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Elit pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis lectus nulla at. Elementum nisi 
+                        quis eleifend quam. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Ac tortor dignissim 
+                        convallis aenean et tortor at risus. Lobortis elementum nibh tellus molestie nunc non blandit. Fermentum dui faucibus 
+                        in ornare quam viverra. Turpis egestas maecenas pharetra convallis. 
+                        Vel facilisis volutpat est velit egestas dui id ornare arcu.
+                         </p> 
+                </div>
+                    
+               
+
+                </div>
+
+                <div class="column">
+
+                <div class="columnAdj2">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Elit pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis lectus nulla at. Elementum nisi 
+                        quis eleifend quam. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Ac tortor dignissim 
+                        convallis aenean et tortor at risus. Lobortis elementum nibh tellus molestie nunc non blandit. Fermentum dui faucibus 
+                        in ornare quam viverra. Turpis egestas maecenas pharetra convallis. 
+                        Vel facilisis volutpat est velit egestas dui id ornare arcu.
+                     </p> 
+                </div>
+
+                <hr class="goldDec2"></hr>
+               
+                    <img src="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/global/25-Campuses_891x891.jpg" width="600" height="500"></img>
+               
+               </div>
+            </div>
         </div>
-        );
-
+     );
 }
 
 export default Home;
