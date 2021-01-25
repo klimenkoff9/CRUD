c3084c5ae1a564d022c8f793d8a0c11668e01de4
dkamalov <dkamalov8@gmail.com>
2021-01-24 20:32:24 -0500

    comit home changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,13 @@ const Home = () => {
             <center><img src="https://www.cuny.edu/wp-content/uploads/sites/4/media-assets/skyline-1200x627_CUNY_Logo.jpg"/></center><br></br>
             
             <center><h1>Welcome to CUNY University! </h1></center>
+
             <hr></hr>
+
+            <img src="https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/home-preview/include-news/year_in_review.png"></img>
+
+            <p float="right"> Giving out vaccines and shit </p>
+
         </div>
         );
 
