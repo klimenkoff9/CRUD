9d3f0667c8b1b9b871ca3f1c276a870654abd8a1
dkamalov <dkamalov8@gmail.com>
2021-01-24 23:36:08 -0500

    Text is not lorem epsom

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,13 +20,13 @@ const Home = () => {
                 </center>
                     <hr class="goldDec"></hr>
                 <div class= "columnAdj">
-                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Elit pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis lectus nulla at. Elementum nisi 
-                        quis eleifend quam. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Ac tortor dignissim 
-                        convallis aenean et tortor at risus. Lobortis elementum nibh tellus molestie nunc non blandit. Fermentum dui faucibus 
-                        in ornare quam viverra. Turpis egestas maecenas pharetra convallis. 
-                        Vel facilisis volutpat est velit egestas dui id ornare arcu.
-                         </p> 
+                <p> 
+                    The nation’s largest urban university, offering a range of undergraduate, graduate degrees and continuing education opportunities
+                    to over 500,000 students of all ages and backgrounds on 25 NYC campuses.
+                    25 campuses located across one of the international capitals of the world. With cutting edge technologies, labs,
+                    and equipment. Along with a diverse academic staff that stand at the forefront of their respective fields. Join us to learn 
+                    to become a leader in your field and become part of the bright future of New York City. The city where if you can make it 
+                    here than you can make it anywhere.</p> 
                 </div>
                     
                
@@ -36,12 +36,11 @@ const Home = () => {
                 <div class="column">
 
                 <div class="columnAdj2">
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Elit pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis lectus nulla at. Elementum nisi 
-                        quis eleifend quam. Orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Ac tortor dignissim 
-                        convallis aenean et tortor at risus. Lobortis elementum nibh tellus molestie nunc non blandit. Fermentum dui faucibus 
-                        in ornare quam viverra. Turpis egestas maecenas pharetra convallis. 
-                        Vel facilisis volutpat est velit egestas dui id ornare arcu.
+                  <p> More than 50 students from the City University of New York are winners of prestigious national academic awards. 
+                        Their success reflects the University’s mandate to provide a quality education to students of all backgrounds and socioeconomic circumstances.
+                        Affirming the mission that we sought to uphold in 1874 to bring higher education to previously underserved communities.
+                        We are so incredibly proud of our students and are extremely excited to see what our future graduates achieve.
+
                      </p> 
                 </div>
 
